98773e12bcceef62efe15053207c48d3841e1ed7
maryfranks <maryfranks@gmail.com>
2017-06-26 15:25:24 -0400

    added exercise6.rb and completed if statement

---
+++ exercise6.rb
@@ -0,0 +1,11 @@
+distance = 0
+
+puts "Would you like to walk or run"
+walk_or_run = gets.chomp
+if walk_or_run == "walk"
+  puts "You are #{distance += 1}km from home"
+elsif walk_or_run == "run"
+  puts "You are #{distance += 5}km from home"
+else
+  puts "Please enter either walk or run"
+end
