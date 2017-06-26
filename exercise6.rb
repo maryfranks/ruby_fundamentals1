1879e46282eed73404edf33109e1ff8a72ecaf9e
maryfranks <maryfranks@gmail.com>
2017-06-26 15:42:53 -0400

    added go home option to end game

--- exercise6.rb
+++ exercise6.rb
@@ -6,7 +6,10 @@ if walk_or_run == "walk"
   puts "You are #{distance += 1}km from home"
 elsif walk_or_run == "run"
   puts "You are #{distance += 5}km from home"
+elsif walk_or_run == "go home"
+  puts "Welcome Home!  You travelled #{distance * 2}km today!"
+  break
 else
-  puts "Please enter either walk or run"
+  puts "Please enter either walk, run, or go home"
 end
-end 
+end
