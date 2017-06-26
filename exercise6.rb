5b0f7b8fde6035b68388195790a473120058546e
maryfranks <maryfranks@gmail.com>
2017-06-26 15:58:43 -0400

    added energy limitations to game

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,18 @@
 distance = 0
+energy = 3
 while distance >= 0
 puts "Would you like to walk or run"
 walk_or_run = gets.chomp
 if walk_or_run == "walk"
-  puts "You are #{distance += 1}km from home"
+  energy += 1
+  puts "You are #{distance += 1}km from home!"
 elsif walk_or_run == "run"
-  puts "You are #{distance += 5}km from home"
+  if energy < 2
+    puts "Please walk to regain some energy!"
+  else
+    energy -= 2
+    puts "You are #{distance += 5}km from home"
+  end
 elsif walk_or_run == "go home"
   puts "Welcome Home!  You travelled #{distance * 2}km today!"
   break
