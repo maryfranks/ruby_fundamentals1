4a8160d97315f636368382ec6ba5f3a346d46c4f
maryfranks <maryfranks@gmail.com>
2017-06-26 17:06:47 -0400

    changed variable from walk_or_run to activity

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,5 @@
-puts "It's bright and early in the morning and you decide to get some fresh air!  You can choose to walk or run.  Walking increases your energy and running uses it up.  You can only run as long as you have energy.  If you run out of energy, either walk, rest, or forage for food to get more energy to run. Do you want to play (yes or no)"
-loop do 
+puts "It's bright and early in the morning and you decide to get some fresh air!  You can choose to walk or run.  Walking increases your energy and running uses it up.  You can only run as long as you have energy.  If you run out of energy, either walk, rest, or forage for food to get more energy to run. When you're done playing, you can go home.  Do you want to play? (yes or no)"
+loop do
 desire_to_play = gets.chomp
 
 if desire_to_play == "yes"
@@ -10,21 +10,21 @@ if desire_to_play == "yes"
 
   while distance >= 0
   puts "Would you like to walk or run"
-  walk_or_run = gets.chomp
-  if walk_or_run == "walk"
+  activity = gets.chomp
+  if activity == "walk"
     energy += 1
     puts "You are #{distance += 1}km from home! You have #{energy} energy."
-  elsif walk_or_run == "run"
+  elsif activity == "run"
     if energy < 2
       puts "You have #{energy} energy.  Please walk or rest to regain some energy!"
     else
       energy -= 2
       puts "You are #{distance += 5}km from home! You have #{energy} energy."
     end
-  elsif walk_or_run == "rest"
+  elsif activity == "rest"
     energy += 2
     puts "Whew, that was hard work!  You have #{energy} energy."
-  elsif walk_or_run == "forage"
+  elsif activity == "forage"
     if energy < 1
       puts "You don't have enough energy to look for food.  Please walk or rest to regain energy."
     else
@@ -39,7 +39,7 @@ if desire_to_play == "yes"
         puts "Sorry no food here."
       end
     end
-  elsif walk_or_run == "go home"
+  elsif activity == "go home"
     puts "Welcome Home!  You travelled #{distance}km from home today!"
     break
   else
