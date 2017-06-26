7f8d4e7b1665fc52eb799877d53c91bd5c39316c
maryfranks <maryfranks@gmail.com>
2017-06-26 16:09:52 -0400

    added rest option to regain energy

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,4 @@
+puts "It's bright and early in the morning and you decide to get some fresh air!  You can choose to walk or run.  Walking increases your energy and running uses it up.  You can only run as long as you have energy.  If you run out of energy, either walk or rest to get more energy to run."
 distance = 0
 energy = 3
 while distance >= 0
@@ -6,15 +7,22 @@ walk_or_run = gets.chomp
 if walk_or_run == "walk"
   energy += 1
   puts "You are #{distance += 1}km from home!"
+  # puts "#{energy}"
 elsif walk_or_run == "run"
   if energy < 2
-    puts "Please walk to regain some energy!"
+    puts "Please walk or rest to regain some energy!"
+    # puts "#{energy}"
   else
     energy -= 2
     puts "You are #{distance += 5}km from home"
+    # puts "#{energy}"
   end
+elsif walk_or_run == "rest"
+  energy += 2
+  puts "Whew, that was hard work!"
+  # puts "#{energy}"
 elsif walk_or_run == "go home"
-  puts "Welcome Home!  You travelled #{distance * 2}km today!"
+  puts "Welcome Home!  You travelled #{distance}km from home today!"
   break
 else
   puts "Please enter either walk, run, or go home"
